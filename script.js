c38ed8a310b73431e677713653ff75812ab84dde
Erik Severtson <70035792+ErikSevertson@users.noreply.github.com>
2025-01-25 15:21:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,5 @@ const output = document.getElementById("output");
 button.addEventListener("click", function () {
     x = x + 1;
     output.textContent = x;
-    alert("Button was clicked!");
 });
 
