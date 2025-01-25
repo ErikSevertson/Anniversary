481aae64f7ad2dc10de0954b970ba650c86aea8c
Erik Severtson <70035792+ErikSevertson@users.noreply.github.com>
2025-01-25 15:10:50 -0600

    handles increase

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 let x = 0;
 
 const button = document.getElementById("myButton");
+const output = document.getElementById("output:);
 
 button.addEventListener("click", function () {
     x = x + 1;
+    output.textContent = x;
     alert("Button was clicked!");
 });
 
